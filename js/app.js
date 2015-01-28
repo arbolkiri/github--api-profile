fe28e6e91943b4d5f1f9c1b16f83e159427f2990
Nicole Cano <arbolkiri@gmail.com>
2015-01-28 17:24:10 -0600

    another update

--- js/app.js
+++ js/app.js
@@ -30,10 +30,11 @@ function app() {
             var token = "ebea4cc6152fc438604271db8a796366b4f7a5d7";
             var githubUsername = "arbolkiri";
             var left_template_url = "./templates/leftside.html";
-            // var right_template_url = "./templates/rightside.html";
+            var right_template_url = "./templates/rightside.html";
             var profile_url = "https://api.github.com/users/" + githubUsername + "?access_token=" + token;
+            var repos_url = "https://api.github.com/users/" + githubUsername + "/repos?access_token=" + token;
 
-            // var url2 = "https://api.github.com/users/" + githubUsername + "/repos" + "?access_token=" + token;
+            
 
             var promise1 = $.get(profile_url);
             var promise2 = $.get(left_template_url);
@@ -47,12 +48,21 @@ function app() {
                     var template = arguments[1]
                     document.getElementsByClassName("left")[0].innerHTML += _.template(template[0], data[0]);
                 }
-                // document.getElementsByClassName("left")[0].innerHTML += _.template(template[0], data[0]
-                // "status:",
-                // arguments[1]
+                
+            var promise3 = $.get(repos_url);
+            var promise4 = $.get(right_template_url);
 
+            $.when(promise3, promise4).then(drawRepos)
 
-            // function drawRepos() {}
+            function drawRepos(repo, template) {
+                // console.log(arguments[0])
+                // console.log(arguments[1])
+                debugger;
+                repo[0].forEach(function(value){
+                    // debugger;
+                    document.querySelector(".right").innerHTML += _.template(template[0], value);
+                })
+            }
 
         })
     }
@@ -60,4 +70,4 @@ function app() {
 
 
 
-// var reps = "repos";
+
